1a81ae52631717984591ba3474884938b6ad1c0f
srikanthmerugu <srikanthmerugu0@gmail.com>
2025-01-11 20:34:55 +0530

    About Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Header from './components/Header'
+import About from './components/About'
 
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Header />
+      <About/>
+    
     </div>
   )
 }
