a87edc75e2fcfceecea161084a0b2ce738ac0e23
srikanthmerugu <srikanthmerugu0@gmail.com>
2025-01-31 00:08:37 +0530

    project details added

--- src/images/index.js
+++ src/images/index.js
@@ -2,9 +2,10 @@ export {default as profile1} from "./Sree-profile.png"
 // export {default as profile2} from "./profile-2.jpg"
 export {default as profile2} from "./Sree-profile.png"
 export {default as profile3} from "./edulyn.png"
-export {default as MyResume} from "./Srikanth Merugu Resume.pdf"
+export {default as MyResume} from "./Srikanth Merugu - Resume.pdf"
 export {default as NewLogo} from "./New-Log0.png"
 export {default as matchoo} from  "./matchoo.png"
 export {default as Smartuter} from  "./Smartuter.png"
 export {default as Vetoro} from  "./Vetoro.png"
 export {default as juice} from './juice.png'
+export {default as FEVICON} from './FEVICON.jpg'
